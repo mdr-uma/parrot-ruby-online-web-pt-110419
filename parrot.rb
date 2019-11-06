2291412d2280948dc1a3f752f67d37d6ad74ba6b
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-06 19:44:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,7 +2,10 @@
 # returns the phrase
 def parrot(parrot = "Pretty bird!")
   puts "Squawk!"
-  puts "Pretty bird!"
     "Squawk!"
-    "Pretty bird!"
+end
+
+def parrot
+  puts "Pretty bird!"
+  "Pretty bird!"
 end
